3f89c738373ab8e30097a45f95e29dd0f4c72d6f
Ihor Nahnybida <igor653279@gmail.com>
2021-08-29 17:06:40 +0300

    Updates

---
+++ precache-manifest.185d16f03ee71a56cd3e39f4d61f54cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0359be50a5573e7918dfd536a91476b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8ebb46c4c07384e17d3",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ebb46c4c07384e17d3",
+    "url": "./static/js/main.98df7a96.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
